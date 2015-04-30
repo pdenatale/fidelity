ccdb241313ad5b9a8177bce14954d47baf0e73b6
gmaggiotti <gabriel.maggiotti@mercadolibre.com>
2015-04-30 17:41:43 -0300

    changing db user

--- htdocs/dbConn.php
+++ htdocs/dbConn.php
@@ -1,8 +1,7 @@
 <?php
 $username = "root";
-$password = "tribilin78";
+$password = "";
 $hostname = "localhost"; 
-
 //connection to the database
 $dbhandle = mysql_connect($hostname, $username, $password)
   or die("Unable to connect to MySQL");
